a01455c028a2418f8b63e55ff1aef6128bf67151
Alemiz <alex@mizerak.eu>
2025-02-12 14:08:59 +0100

    Add Builder to creative item classes

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/data/inventory/CreativeItemData.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/data/inventory/CreativeItemData.java
@@ -1,8 +1,12 @@
 package org.cloudburstmc.protocol.bedrock.data.inventory;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
 public class CreativeItemData {
     private final ItemData item;
     private final int netId;
